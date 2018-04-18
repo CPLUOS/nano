1d1792e157e05fd1af516688f773f9b6f3dc1721
dj jeon <xicuvybgh@gmail.com>
2018-04-18 20:48:27 +0900

    update analyser (and its header) for new sample

--- analysis/bin/vtsAnalysis.cc
+++ analysis/bin/vtsAnalysis.cc
@@ -13,10 +13,38 @@ TParticle vtsAnalysis::GetTParticle(int pdgId, int idx) {
     TLorentzVector tlv;
     if      (abs(pdgId) == 11) tlv.SetPtEtaPhiM(Electron_pt[idx], Electron_eta[idx], Electron_phi[idx], Electron_mass[idx]);
     else if (abs(pdgId) == 13) tlv.SetPtEtaPhiM(Muon_pt[idx], Muon_eta[idx], Muon_phi[idx], Muon_mass[idx]);
+    else if (abs(pdgId) > 100) tlv.SetPtEtaPhiM(had_pt[idx], had_eta[idx], had_phi[idx], had_mass[idx]);
     else tlv.SetPtEtaPhiM(Jet_pt[idx], Jet_eta[idx], Jet_phi[idx], Jet_mass[idx]);
 
     return TParticle(pdgId, 0, 0, 0, 0, 0, tlv, tlv);
 }
+
+//Double_t vtsAnalysis::DeltaR(Double_t deta, Double_t dphi) {
+//  return TMath::Sqrt(deta*deta + dphi*dphi);
+//}
+
+Double_t vtsAnalysis::DeltaPhi(Double_t phi1, Double_t phi2) {
+  static const Double_t kPI = TMath::Pi();
+  static const Double_t kTWOPI = 2*TMath::Pi();
+  Double_t x = phi1 - phi2;
+  if(TMath::IsNaN(x)){
+    std::cerr << "DeltaPhi function called with NaN" << std::endl;
+    return x;
+  }
+  while (x >= kPI) x -= kTWOPI;
+  while (x < -kPI) x += kTWOPI;
+  return x;
+}
+
+Double_t vtsAnalysis::GetD(float pt, float eta, float phi, float m, float vx, float vy, float vz) {
+  TLorentzVector tlv;
+  tlv.SetPtEtaPhiM(pt,eta,phi,m);
+  std::vector<Double_t> b = { tlv.Px()/(sqrt(tlv.Px()*tlv.Px()+tlv.Py()*tlv.Py()+tlv.Pz()*tlv.Pz())), tlv.Py()/(sqrt(tlv.Px()*tlv.Px()+tlv.Py()*tlv.Py()+tlv.Pz()*tlv.Pz())), tlv.Pz()/(sqrt(tlv.Px()*tlv.Px()+tlv.Py()*tlv.Py()+tlv.Pz()*tlv.Pz())) };
+  std::vector<Double_t> a = { vx - PV_x, vy - PV_y, vz - PV_z };
+  std::vector<Double_t> cross = { a[1]*b[2]-a[2]*b[1], a[2]*b[0]-a[0]*b[2], a[0]*b[1]-a[1]*b[0] };
+  return sqrt( cross[0]*cross[0] + cross[1]*cross[1] + cross[2]*cross[2] );
+}
+
 
 int main(Int_t argc, Char_t** argv){
 
@@ -24,12 +52,9 @@ int main(Int_t argc, Char_t** argv){
 
   if(argc <= 1){
     cout << "no input file is specified. running with default file." << endl;
-    auto inFile = TFile::Open("nanoAOD_forsync.root", "READ");
-    //auto inFile = TFile::Open("/xrootd/store/user/tt8888tt/nanoAOD_forsync.root", "READ");
+    auto inFile = TFile::Open("/xrootd/store/group/nanoAOD/run2_2016v4/tsw/nanoAOD_1.root", "READ");
     auto inTree = (TTree*) inFile->Get("Events");
-    cout << "c " << inTree << endl;
     vtsAnalysis ana(inTree);
-    //vtsAnalysis ana(0);
     ana.MakeTree("nanotree.root");
     ana.Loop();
   }
@@ -40,6 +65,7 @@ int main(Int_t argc, Char_t** argv){
     string outFileDir = hostDir+getenv("USER")+"/"+jobName+"/"+sampleName;
     for(Int_t i = 3; i < argc; i++){
       auto inFileName = argv[i];
+      cout << inFileName << endl;
       TFile *inFile = TFile::Open(inFileName, "READ");
       TTree *inTree = (TTree*) inFile->Get("Events");
       vtsAnalysis ana(inTree);
@@ -62,12 +88,13 @@ void vtsAnalysis::Loop(){
     fChain->GetEntry(entry);
     if (iev%10000 == 0) cout << iev << "/" << nentries << endl;
 
-    //ResetBranch();
-    //EventSelection();
-    //outTree->Fill();
+    ResetBranch();
+    FindQuark();
+    KshortAnalysis();
+    EventSelection();
+    outTree->Fill();
   }
 
-  for (int i =1; i<10; i++){ cout << h_cutFlow->GetBinContent(i) << "  "; }
   outFile->Write();
   outFile->Close();
   cout << endl;
@@ -81,6 +108,15 @@ void vtsAnalysis::MakeTree(std::string outFileName){
   outTree->Branch("njet", &b_njet, "njet/I");
   outTree->Branch("met", &b_met, "met/F");
   outTree->Branch("dilep_tlv", "TLorentzVector", &b_dilep_tlv);
+
+  outTree->Branch("KS_tlv", "TLorentzVector", &b_KS_tlv);
+  outTree->Branch("isFrom_KS", &b_isFrom_KS, "isFrom_KS/I");
+  outTree->Branch("d_KS", &b_d_KS , "d_KS/F" );
+  outTree->Branch("x_KS", &b_x_KS, "x_KS/F");
+  outTree->Branch("angleXY_KS", &b_angleXY_KS, "angleXY_KS/F");
+  outTree->Branch("lxySig_KS", &b_lxySig_KS, "lxySig_KS/F");
+  outTree->Branch("chi2_KS", &b_chi2_KS, "chi2_KS/F");
+  outTree->Branch("dca_KS", &b_dca_KS, "dca_KS/F");
 }
 
 void vtsAnalysis::ResetBranch(){
@@ -89,17 +125,97 @@ void vtsAnalysis::ResetBranch(){
   b_dilep_tlv.SetPtEtaPhiM(0,0,0,0);
   recoleps.clear();
 
+  b_KS_tlv.SetPtEtaPhiM(0,0,0,0);
+  b_isFrom_KS = -9;
+  b_d_KS = -1; b_x_KS = -1;
+  b_angleXY_KS = -1; b_lxySig_KS = -1; b_chi2_KS = -1; b_dca_KS = -1;
+
+  isS_ = false;
+  q_ = -1; qb_ = -1;
+  qj_ = -1; qbj_ = -1;
+}
+
+void vtsAnalysis::KshortAnalysis(){
+  if (q_ == -1) return;
+
+  TLorentzVector q_tlv;
+  q_tlv.SetPtEtaPhiM(GenPart_pt[q_], GenPart_eta[q_], GenPart_phi[q_], GenPart_mass[q_]);
+  TLorentzVector qb_tlv;
+  qb_tlv.SetPtEtaPhiM(GenPart_pt[qb_], GenPart_eta[qb_], GenPart_phi[qb_], GenPart_mass[qb_]);
+  for(unsigned int j=0; j<nJet;++j){
+    TLorentzVector jet_tlv;
+    jet_tlv.SetPtEtaPhiM(Jet_pt[j], Jet_eta[j], Jet_phi[j], Jet_mass[j]);
+    if ( q_tlv.DeltaR(jet_tlv) < 0.3) qj_ = j;
+    if ( qb_tlv.DeltaR(jet_tlv) < 0.3) qbj_ = j;
+  }
+  if (qj_ == -1 || qbj_ == -1) return;
+  //if (qj_ == -1 || qbj_ == -1) {cout << "No reco jet matched with gen s/b" << endl; return;}
+
+  TLorentzVector qj_tlv;
+  qj_tlv.SetPtEtaPhiM(Jet_pt[qj_], Jet_eta[qj_], Jet_phi[qj_], GenPart_mass[qj_]);
+  TLorentzVector qbj_tlv;
+  qbj_tlv.SetPtEtaPhiM(Jet_pt[qbj_], Jet_eta[qbj_], Jet_phi[qbj_], GenPart_mass[qbj_]);
+
+  std::vector<struct HadStat> HadInJet;
+  struct HadStat Stat;
+  //Reconstructed hadrons
+  for(unsigned int k=0; k<nhad; ++k){
+    if (had_lxy[k]/had_lxyErr[k]>3 && had_angleXY[k]>0.95 && had_chi2[k]<5 &&had_dca[k]<2){ 
+      TLorentzVector had_tlv;
+      had_tlv.SetPtEtaPhiM(had_pt[k], had_eta[k], had_phi[k], had_mass[k]);
+      if (abs(had_pdgId[k]) != 310 && abs(had_pdgId[k]) != 3122) continue;
+      auto dr = had_tlv.DeltaR(qj_tlv);
+      auto drb = had_tlv.DeltaR(qbj_tlv);
+      if(dr < 0.3 && (had_pt[k]/Jet_pt[qj_]) > 0.15){
+        Stat.idx = k;
+        Stat.pdgId = had_pdgId[k];
+        Stat.x = had_pt[k]/Jet_pt[qj_];
+        Stat.label = abs(GenPart_pdgId[q_]);
+        HadInJet.push_back(Stat);
+      }
+      else if(drb < 0.3 && (had_pt[k]/Jet_pt[qbj_]) > 0.15){
+        Stat.idx = k;
+        Stat.pdgId = had_pdgId[k];
+        Stat.x = had_pt[k]/Jet_pt[qj_];
+        Stat.label = abs(GenPart_pdgId[q_]);
+        HadInJet.push_back(Stat);
+      }
+    }
+  }
+  if (HadInJet.size() == 0) return;
+  sort(HadInJet.begin(), HadInJet.end(), [](struct HadStat a, struct HadStat b){return a.x > b.x;}); // pick highest one
+  auto idx = HadInJet[0].idx;
+  auto pdgid = HadInJet[0].pdgId;
+  b_KS_tlv.SetPtEtaPhiM(had_pt[idx], had_eta[idx], had_phi[idx], had_mass[idx]);
+
+  if (abs(pdgid) == 310) {
+    b_x_KS = HadInJet[0].x;
+    b_isFrom_KS = HadInJet[0].label;
+    b_d_KS = GetD(had_pt[idx], had_eta[idx], had_phi[idx], had_mass[idx], had_x[idx], had_y[idx], had_z[idx]);
+    b_angleXY_KS = had_angleXY[idx];
+    b_lxySig_KS = had_lxy[idx]/had_lxyErr[idx];
+    b_chi2_KS = had_chi2[idx];
+    b_dca_KS = had_dca[idx];
+  }
+}
+
+void vtsAnalysis::FindQuark(){
+  //Find s/b quark from Gen Info.
+  for(unsigned int i=0; i<nGenPart; ++i){
+    if (std::abs(GenPart_status[i] - 25) < 5 && GenPart_pdgId[i] == 3) { q_ = i; isS_ = true; }
+    if (std::abs(GenPart_status[i] - 25) < 5 && GenPart_pdgId[i] == -3) { qb_ = i; }
+    if (std::abs(GenPart_status[i] - 25) < 5 && GenPart_pdgId[i] == 5) { q_ = i; }
+    if (std::abs(GenPart_status[i] - 25) < 5 && GenPart_pdgId[i] == -5) { qb_ = i; }
+  }
 }
 
 void vtsAnalysis::EventSelection(){
-  h_cutFlow->Fill(0);
   recolep1_tlv.SetPtEtaPhiM(0,0,0,0);
   recolep2_tlv.SetPtEtaPhiM(0,0,0,0);
 
   auto muons = muonSelection();
   auto elecs = elecSelection();
   if(muons.size()+ elecs.size() != 2) return;
-  h_cutFlow->Fill(1);
 
   if      (muons.size() == 2){ recolep1 = muons[0]; recolep2 = muons[1]; b_channel = CH_MUMU; }
   else if (elecs.size() == 2){ recolep1 = elecs[0]; recolep2 = elecs[1]; b_channel = CH_ELEL; }
@@ -114,19 +230,15 @@ void vtsAnalysis::EventSelection(){
   auto jets = jetSelection();
 
   if (b_dilep_tlv.M() < 20.) return;
-  h_cutFlow->Fill(2);
 
   if (b_channel != CH_MUEL && 76 < b_dilep_tlv.M() && b_dilep_tlv.M() < 106) return;
-  h_cutFlow->Fill(3);
 
   b_met = MET_pt;
   b_njet = jets.size();
 
   if (b_channel != CH_MUEL && b_met < 40) return;
-  h_cutFlow->Fill(4);
 
   if (b_njet < 2) return;
-  h_cutFlow->Fill(5);
 
 }
 
