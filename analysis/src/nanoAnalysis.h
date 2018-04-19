2f17811739008a393ff1dea0da3da5e987de08a0
jang00777 <jang00777@naver.com>
2018-04-19 23:35:49 +0900

    Added had vars and lambda

--- analysis/src/nanoAnalysis.h
+++ analysis/src/nanoAnalysis.h
@@ -15,98 +15,98 @@
 #include "TTbarModeDefs.h"
 
 void makeEventsClass(const char* filedir){
-    TFile *f = TFile::Open(filedir);
-    TTree *t = (TTree*) f->Get("Events");
-    t->MakeClass(); // this will generate Events.h file.
+  TFile *f = TFile::Open(filedir);
+  TTree *t = (TTree*) f->Get("Events");
+  t->MakeClass(); // this will generate Events.h file.
 }
 
 #ifdef nanoAnalysis_cxx
 class nanoAnalysis : public Events {
 private: 
-      //Output Variables
-      TFile* m_output;
-      
-      //Tree
-      TTree* m_tree;
-      
-      //histogram
-      TH1D* h_Event_Tot;
-      TH1D* h_genweights;
-      TH1D* h_weight;
-      TH1D* h_FL;
-      TH1D* h_SL;
-      TH1D* h_FH2;
-      TH1D* h_FH3;
-      TH1D* h_FH4;
-      TH1D* h_Out;
+  //Output Variables
+  TFile* m_output;
+  
+  //Tree
+  TTree* m_tree;
+  
+  //histogram
+  TH1D* h_Event_Tot;
+  TH1D* h_genweights;
+  TH1D* h_weight;
+  TH1D* h_FL;
+  TH1D* h_SL;
+  TH1D* h_FH2;
+  TH1D* h_FH3;
+  TH1D* h_FH4;
+  TH1D* h_Out;
+
+  //Variables
+  TLorentzVector b_Dilep, b_Mu1, b_Mu2;
+  TLorentzVector b_lep1, b_lep2;
+
+  std::vector<TLorentzVector> b_Mu_tlv, b_El_tlv, b_Jet_tlv, b_bJet_tlv;
+  
+  std::vector<Float_t> b_CSVv2;
+  std::vector<Float_t> b_csvweights;
+  
+  Float_t b_genweight, b_weight;
+  Float_t b_puweight, b_puweight_up, b_puweight_dn; 
+  Float_t b_mueffweight, b_mueffweight_up, b_mueffweight_dn;
+  Float_t b_btagweight;
+
+  Int_t b_Event_No, b_Event_Total;
+  
+  Float_t b_Met_phi;
+  Float_t b_Met;
+  Int_t b_npvs;
+
+  //Step and Cutflow
+  TH1D* h_cutFlow;
+  Int_t b_Step;
  
-      //Variables
-      TLorentzVector b_Dilep, b_Mu1, b_Mu2;
-      TLorentzVector b_lep1, b_lep2;
+  //Channel
+  Int_t b_channel, b_nlep, b_nmuon, b_nelec, b_njet, b_nbjet, b_charge;
+  Bool_t keep;
+  //triggers
+  Bool_t b_trig_m, b_trig_m2,  b_trig_e, b_trig_mm, b_trig_em, b_trig_ee;
+  Int_t b_FL, b_FH2, b_FH3, b_FH4, b_SL;
 
-      std::vector<TLorentzVector> b_Mu_tlv, b_El_tlv, b_Jet_tlv, b_bJet_tlv;
-      
-      std::vector<Float_t> b_CSVv2;
-      std::vector<Float_t> b_csvweights;
-      
-      Float_t b_genweight, b_weight;
-      Float_t b_puweight, b_puweight_up, b_puweight_dn; 
-      Float_t b_mueffweight, b_mueffweight_up, b_mueffweight_dn;
-      Float_t b_btagweight;
+  //Tools
+  pileUpTool* m_pileUp;
+  lumiTool* m_lumi;
+  RoccoR* m_rocCor;
+  MuonScaleFactorEvaluator m_muonSF;
+  ElecScaleFactorEvaluator m_elecSF;
+  BTagWeightEvaluator m_btagSF;
+  std::vector<UInt_t> idxs;
+  
+  //Making output branch
+  void MakeBranch(TTree* t);
+  void ResetBranch();
 
-      Int_t b_Event_No, b_Event_Total;
-      
-      Float_t b_Met_phi;
-      Float_t b_Met;
-      Int_t b_npvs;
+  bool Analysis();
+  //For Selection
+  Bool_t LumiCheck();
+  enum TTLLChannel { CH_NOLL = 0, CH_MUEL, CH_ELEL, CH_MUMU };
 
-      //Step and Cutflow
-      TH1D* h_cutFlow;
-      Int_t b_Step;
-     
-      //Channel
-      Int_t b_channel, b_nlep, b_nmuon, b_nelec, b_njet, b_nbjet, b_charge;
-      Bool_t keep;
-      //triggers
-      Bool_t b_trig_m, b_trig_m2,  b_trig_e, b_trig_mm, b_trig_em, b_trig_ee;
-      Int_t b_FL, b_FH2, b_FH3, b_FH4, b_SL;
-
-      //Tools
-      pileUpTool* m_pileUp;
-      lumiTool* m_lumi;
-      RoccoR* m_rocCor;
-      MuonScaleFactorEvaluator m_muonSF;
-      ElecScaleFactorEvaluator m_elecSF;
-      BTagWeightEvaluator m_btagSF;
-      std::vector<UInt_t> idxs;
-      
-      //Making output branch
-      void MakeBranch(TTree* t);
-      void ResetBranch();
-
-      bool Analysis();
-      //For Selection
-      Bool_t LumiCheck();
-      enum TTLLChannel { CH_NOLL = 0, CH_MUEL, CH_ELEL, CH_MUMU };
-
-      bool hasOverLap(TLorentzVector cand, vector<TParticle> objects, Float_t rad);
-      
-      std::vector<TParticle> MuonSelection();
-      std::vector<TParticle> ElectronSelection(std::vector<TParticle>);
-      std::vector<TParticle> JetSelection(std::vector<TParticle>, std::vector<TParticle>);
-      std::vector<TParticle> BJetSelection(std::vector<TParticle>);
-      
-      Double_t roccoR(TLorentzVector m, Int_t &q, Int_t &nGen, Int_t &nTrackerLayers);
+  bool hasOverLap(TLorentzVector cand, vector<TParticle> objects, Float_t rad);
+  
+  std::vector<TParticle> MuonSelection();
+  std::vector<TParticle> ElectronSelection(std::vector<TParticle>);
+  std::vector<TParticle> JetSelection(std::vector<TParticle>, std::vector<TParticle>);
+  std::vector<TParticle> BJetSelection(std::vector<TParticle>);
+  
+  Double_t roccoR(TLorentzVector m, Int_t &q, Int_t &nGen, Int_t &nTrackerLayers);
 
 public:
-      Bool_t m_isMC;
+  Bool_t m_isMC;
 
-      //set output file
-      void SetOutput(std::string outputName);
-      void LoadModules(pileUpTool* pileUp, lumiTool* lumi, RoccoR* rocCor);
-      nanoAnalysis(TTree *tree=0, Bool_t isMc = false);
-      ~nanoAnalysis();
-      virtual void     Loop();
+  //set output file
+  void SetOutput(std::string outputName);
+  void LoadModules(pileUpTool* pileUp, lumiTool* lumi, RoccoR* rocCor);
+  nanoAnalysis(TTree *tree=0, Bool_t isMc = false);
+  ~nanoAnalysis();
+  virtual void     Loop();
 };
 
 nanoAnalysis::nanoAnalysis(TTree *tree, Bool_t isMC) : Events(tree), m_isMC(isMC)
@@ -123,60 +123,68 @@ nanoAnalysis::~nanoAnalysis()
 #ifdef vtsAnalysis_cxx
 class vtsAnalysis : public Events {
 private:
-      TFile* outFile; TTree* outTree;
+  TFile* outFile; TTree* outTree;
 
-      TH1D* h_cutFlow;
-      TParticle recolep1, recolep2;
-      TLorentzVector recolep1_tlv, recolep2_tlv;
-      std::vector<TLorentzVector> recoleps;
-      //std::vector<TLorentzVector> recoleps[recolep1_tlv,recolep2_tlv];
-      //std::vector<TLorentzVector> recoleps(2);
+  TH1D* h_cutFlow;
+  TParticle recolep1, recolep2;
+  TLorentzVector recolep1_tlv, recolep2_tlv;
+  std::vector<TLorentzVector> recoleps;
+  //std::vector<TLorentzVector> recoleps[recolep1_tlv,recolep2_tlv];
+  //std::vector<TLorentzVector> recoleps(2);
 
-      int b_channel;
-      int b_njet;
-      float b_met;
-      TLorentzVector b_dilep_tlv;
-      enum TTLLChannel { CH_NOLL = 0, CH_MUEL, CH_ELEL, CH_MUMU };
+  int b_channel;
+  int b_njet;
+  float b_met;
+  TLorentzVector b_dilep_tlv;
+  enum TTLLChannel { CH_NOLL = 0, CH_MUEL, CH_ELEL, CH_MUMU };
 
-      TLorentzVector b_KS_tlv;
-      int b_isFrom_KS;
-      float b_d_KS, b_x_KS;
-      float b_angleXY_KS, b_lxySig_KS, b_chi2_KS, b_dca_KS;
+  TLorentzVector b_had_tlv;
+  int b_isFrom_had;
+  float b_d_had, b_x_had;
+  float b_lxy_had, b_lxySig_had, b_angleXY_had, b_angleXYZ_had, b_chi2_had, b_dca_had;
+  float b_pt_had, b_eta_had, b_l3D_had, b_l3DSig_had, b_legDR_had, b_mass_had; 
+  int b_pdgId_had; 
+  float b_dau1_chi2_had, b_dau1_ipsigXY_had, b_dau1_ipsigZ_had, b_dau1_pt_had;
+  float b_dau2_chi2_had, b_dau2_ipsigXY_had, b_dau2_ipsigZ_had, b_dau2_pt_had;
 
-      bool isS_;
-      int q_, qb_;
-      int qj_, qbj_;
+  std::map<unsigned int, int> qjMapForMC_;
+  std::vector<int> qMC_;
+  std::vector<int> genJet_;           
+  std::vector<int> recoJet_;
 
-      //functions
-      void ResetBranch();
+  //functions
+  void EventSelection();
+  void ResetBranch();
 
-      void KshortAnalysis();
-      void FindQuark();
-      void Gen();
-      Double_t DeltaR(Double_t deta, Double_t dphi);
-      Double_t DeltaPhi(Double_t phi1, Double_t phi2);
-      Double_t GetD(float pt, float eta, float phi, float m, float vx, float vy, float vz);
+  void MatchingForMC();
+  void HadronAnalysis();
 
-      void EventSelection();
-      TParticle GetTParticle(int pdgId, int idx);
-      std::vector<TParticle> muonSelection();
-      std::vector<TParticle> elecSelection();
-      std::vector<TParticle> jetSelection();
+  TParticle GetTParticle(int pdgId, int idx);
 
-      struct HadStat {
-        int idx = -1;
-        int pdgId = -99;
-        float x = -1;
-        int label = -9;
-      };
+  Double_t DeltaR(Double_t deta, Double_t dphi); 
+  Double_t DeltaPhi(Double_t phi1, Double_t phi2);
+  Double_t GetD(float pt, float eta, float phi, float m, float vx, float vy, float vz);
+
+  std::vector<TParticle> muonSelection();
+  std::vector<TParticle> elecSelection();
+  std::vector<TParticle> jetSelection();
+
+  struct HadStat {
+    int idx = -1;
+    int pdgId = -99;
+    float x = -1;
+    int label = -9;
+  };
 
 public:
-      void MakeTree(std::string outputName);
-      vtsAnalysis(TTree *tree=0);
-      ~vtsAnalysis();
-      virtual void     Loop();
+  Bool_t isMC_ = false;
+  void MakeTree(std::string outputName);
+  vtsAnalysis(TTree *tree=0);
+  ~vtsAnalysis();
+  virtual void     Loop();
 };
-vtsAnalysis::vtsAnalysis(TTree *tree) : Events(tree){}
+vtsAnalysis::vtsAnalysis(TTree *tree) : Events(tree)
+{ }
 vtsAnalysis::~vtsAnalysis(){ }
 #endif
 
